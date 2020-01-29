5ccb923cbf9f964f1d18867d59d30e0368a8ea11
ron sutherland <ronald.sutherland@gmail.com>
2020-01-28 23:16:26 -0700

    license of derived can not be changed

--- Manager/lib/uart.c
+++ Manager/lib/uart.c
@@ -1,23 +1,23 @@
 /*
     AVR Interrupt-Driven UART with stdio redirect
-    Copyright (C) 2019 Ronald Sutherland
+    Copyright (C) 2020 Ronald Sutherland
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+For a copy of the GNU General Public License use
+http://www.gnu.org/licenses/gpl-2.0.html
 
     Hacked from UART library by Andy Gock
     https://github.com/andygock/avr-uart
+    which was derived by code from Peter Fleury (and that was GPL so this must be also)
 
     Added Streams from Steve Rodgerson
     https://github.com/hwstar
